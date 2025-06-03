a3917e51ef12f4a23a8fc94e119a8b1504540be9
Leandro Franklin <leandrofrankss5@gmail.com>
2025-06-03 11:03:28 -0300

    Criação do ProductRepositoryTests e deleteShouldDeleteObjectWhenIdExists()

---
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long exintingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        exintingId = 1L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+
+        repository.deleteById(exintingId);
+
+        Optional<Product> result = repository.findById(exintingId);
+        Assertions.assertFalse(result.isPresent());
+
+    }
+
+}
+
